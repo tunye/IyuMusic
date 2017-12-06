c26d6cabcf901e2d9a853cc506a9f2af9eae08ea
chentong1 <chentong1@staff.weibo.com>
2017-12-06 20:55:06 +0800

    更新jar

---
+++ app/src/main/java/com/iyuba/music/widget/original/HighLightTextCallBack.java
@@ -0,0 +1,9 @@
+package com.iyuba.music.widget.original;
+
+/**
+ * Created by iyuba on 2017/12/4.
+ */
+
+public interface HighLightTextCallBack {
+    void getCurrentHighLightText(String content);
+}
